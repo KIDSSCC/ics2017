ea5a826b3c3fa10cd58aa2dbf08f68cc7df19f78
tracer-ics2017 <tracer@njuics.org>
2024-02-28 03:24:55 -0800

    >  compile 2011360 kidsscc Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  03:24:55 up  1:04,  2 users,  load average: 0.00, 0.00, 0.00 c340cf00ae594bf584a836d0d54fc3514dc34a4d

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,10 +1,11 @@
+#include<stdio.h>
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-
+  printf("the is_batch_mode is:%d",is_batch_mode);
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
