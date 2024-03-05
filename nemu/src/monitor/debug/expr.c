5a7b8da0782b41227b1ed0687089583dd22c96a3
tracer-ics2017 <tracer@njuics.org>
2024-03-05 04:55:54 -0800

    >  compile 2011360 kidsscc Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:55:54 up  1:56,  1 user,  load average: 0.16, 0.03, 0.01 14ae99cc6e189ce3bb304d9b4cb036196ce69ce

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,6 +5,7 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
 
 enum {
   TK_NOTYPE = 256, TK_EQ,
@@ -203,14 +204,180 @@ bool check_parentheses(int p, int q, int* errorType)
 	return (int)true;
 }
 
-uint32_t expr(char *e, bool *success) {
+uint32_t regJudge(char* name, int *errorType)
+{
+	if(strcmp(name, "$eip")==0){
+		return cpu.eip;
+	}
+	for(int i=0;i<8;i++){
+		if(strcmp(name+1, regsl[i])==0){
+			return reg_l(i);
+		}
+	}
+	for(int i=0;i<8;i++){
+		if(strcmp(name+1, regsw[i])==0){
+			return reg_w(i);
+		}
+	}
+	for(int i=0;i<8;i++){
+		if(strcmp(name+1, regsb[i])==0){
+			return reg_b(i);
+		}
+	}
+	*errorType=4;
+	return 0;
+}
+int confirmDomainOperator(int p, int q){
+	int numOfBr = 0;
+	int position = p-1;
+	for(int i=p;i<=q;i++){
+		if(tokens[i].type==TK_NUM){
+			continue;
+		}else if(tokens[i].type==TK_LB){
+			numOfBr++;
+		}else if(tokens[i].type==TK_RB){
+			numOfBr--;
+		}else if((tokens[i].type=='+')||(tokens[i].type=='-')||(tokens[i].type=='*')||(tokens[i].type=='/')||(tokens[i].type==TK_EQ)||(tokens[i].type==TK_NEQ)||(tokens[i].type==TK_AND)||(tokens[i].type==TK_OR)){
+			if(numOfBr!=0){
+				continue;
+			}
+			if(position==p-1){
+				position=i;
+			}else {
+				if((tokens[position].type==TK_EQ)||(tokens[position].type==TK_NEQ)||(tokens[position].type==TK_AND)||(tokens[position].type==TK_OR)){
+					if((tokens[i].type==TK_EQ)||(tokens[i].type==TK_NEQ)||(tokens[i].type==TK_AND)||(tokens[i].type==TK_OR)){
+						position = i;
+					}
+				}else if((tokens[position].type=='+')||(tokens[position].type=='-')){
+					if((tokens[i].type!='*')&&(tokens[i].type!='/')){
+						position = i;
+					}
+				}else{
+					position = i;
+				}
+			}
+		}
+	}
+	return position;
+}
+
+uint32_t eval(int p, int q, int *errorType)
+{
+	if(p > q){
+		*errorType = 1;
+		return 0;
+	}else if(p == q){
+		//curr position is a number
+		char * tmp;
+		if(tokens[p].type == TK_NUM){
+			return (int)strtol(tokens[p].str,&tmp,10);
+		}else if(tokens[p].type == TK_0xNUM){
+			return (int)strtol(tokens[p].str,&tmp,16);
+		}else if(tokens[p].type == TK_REG){
+			return (uint32_t)regJudge(tokens[p].str, errorType);
+		}
+	}else {
+		int tryToCheckParentheses = 0;
+		bool checkResult = check_parentheses(p, q, &tryToCheckParentheses);
+		if(tryToCheckParentheses==0){
+			//the tryToCheckParentheses==0,there were no fatal error in current expr
+			//current expr can continue to calculator according to the checkResult
+			if(checkResult){
+			       int result = eval(p+1, q-1, errorType);
+			       if(*errorType==0){
+				       return result;
+			       }else{
+				       return 0;
+			       }
+			}else {
+				//confirm dominant operator
+				//from p to q
+				int position = confirmDomainOperator(p, q);
+				if(position!=p-1){
+					//find a domain operator
+					int val1 = eval(p, position - 1, errorType);
+					if(*errorType!=0){
+						return 0;
+					}
+					int val2 = eval(position + 1, q, errorType);
+					if(*errorType!=0){
+						return 0;
+					}
+					switch(tokens[position].type){
+						case '+':return val1+val2;
+						case '-':return val1-val2;
+						case '*':return val1*val2;
+						case '/':return val1/val2;
+						case TK_EQ:return val1==val2?1:0;
+						case TK_NEQ:return val1!=val2?1:0;
+						case TK_AND:return val1*val2!=0?1:0;
+						case TK_OR:return ((val1==0)&&(val2==0))?0:1;
+					}
+				}else {
+					if(tokens[p].type==TK_NEGATIVE){
+						int val = eval(p+1, q, errorType);
+						if(*errorType!=0){
+							return 0;
+						}
+						return -val;
+					}else if(tokens[p].type==TK_DERE){
+						int val = eval(p+1, q, errorType);
+						if(*errorType!=0){
+							return 0;
+						}
+						if(val>=0x8000000){
+							*errorType=5;
+							return 0;
+						}
+						return vaddr_read(val, 4);
+					}else if(tokens[p].type==TK_NOT){
+						int val = eval(p+1, q, errorType);
+						if(*errorType!=0){
+							return 0;
+						}
+						return val==0?1:0;
+					}
+				}
+			}
+		}else{
+			//tryToCheckParentheses=1:RB more than LB
+			//tryToCheckParentheses=2:LB more than RB
+			*errorType=tryToCheckParentheses+1;
+			return 0;
+		}
+	}
+	return 0;
+}
+
+bool certainType(int type){
+	if((type!=TK_NUM)&&(type!=TK_RB)&&(type!=TK_REG)){
+		return true;
+	}
+	return false;
+}
+uint32_t expr(char *e, int *success) {
   if (!make_token(e)) {
-    *success = false;
+    *success = 1;
     return 0;
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
+  //TODO();
+  for(int i=0;i<nr_token;i++){
+	  if((tokens[i].type=='-')&&(i==0||certainType(tokens[i-1].type))){
+		  tokens[i].type=TK_NEGATIVE;
+	  }
+	  if((tokens[i].type=='*')&&(i==0||certainType(tokens[i-1].type))){
+		  tokens[i].type=TK_DERE;
+	  }
+  }
+  int tryToEval = 0;
+  int result = eval(0, nr_token-1, &tryToEval);
+  if(tryToEval==0){
+	  return result;
+  }else{
+	  *success = result+1;
+	  return 0;
+  }
   return 0;
 }
