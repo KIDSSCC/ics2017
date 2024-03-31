0cd11575721bc3b02154459073f9d90364b6f010
tracer-ics2017 <tracer@njuics.org>
2024-03-31 01:10:16 -0700

    >  run 2011360 kidsscc Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:10:16 up  2:03,  4 users,  load average: 0.10, 0.12, 0.09 cd33d75c9263146228caa6b6fa65907b83e8895a

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -5,7 +5,9 @@ int main(){
   _ioe_init();
   int sec = 1;
   while (1) {
-    while(_uptime() < 1000 * sec);
+    while(_uptime() < 1000 * sec){
+	    printf("wait\n");
+    }
     if (sec == 1) {
       printf("%d second.\n", sec);
     } else {
