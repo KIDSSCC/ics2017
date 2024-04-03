8d24be972eaae66e5709d4b1322e8d05992f518d
tracer-ics2017 <tracer@njuics.org>
2024-04-03 06:54:53 -0700

    >  compile 2011360 kidsscc Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  06:54:53 up 40 min,  4 users,  load average: 0.00, 0.05, 0.07 2bc7d93814c81188b1fd5919369b51423059afd2

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -1,4 +1,5 @@
 #include "cpu/exec.h"
+#include <stdio.h>
 
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
@@ -50,7 +51,12 @@ make_EHelper(in) {
   operand_write(id_dest, &t0);
 
   print_asm_template2(in);
-  diff_test_skip_qemu();
+  FILE *file;
+  char *filename = "/home/kidsscc/ownlog.txt";
+  file = fopen(filename, "a");
+  fprintf(file, "current eip is: %08x, and CF is: %d, ZF is: %d, SF is: %d, IF is: %d, OF is: %d\n", cpu.eip, cpu.eflags.CF&0x1, cpu.eflags.ZF&0x01, cpu.eflags.SF&0x01, cpu.eflags.IF&0x01, cpu.eflags.OF&0x01);
+  fclose(file);
+
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();
